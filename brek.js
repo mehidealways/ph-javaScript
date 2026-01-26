50a54092c6f297061aeea5d2a32412f1d962db63
mehidealways <mehidehasanmilon444@.com>
2026-01-26 22:59:43 +0600

    how does work break in while and for loop

---
+++ brek.js
@@ -0,0 +1,19 @@
+
+// How does work break in for loop
+// for (let i = 0; i < 150; i++){
+//   console.log(i);
+//   if (i >= 5) {
+//     break
+//   }
+// }
+// console.log('Life after brek UP');
+
+// How does work break in While loop
+let n = 54;
+while (n > 25) {
+  console.log(n);
+  if (n < 50) {
+    break
+  }
+  n--;
+}
