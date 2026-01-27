1a1b3366fbbdd18731bb1b05cd5ff15562ad098d
mehidealways <mehidehasanmilon444@gmail.com>
2026-01-27 20:39:18 +0400

    how does work String

---
+++ case.js
@@ -0,0 +1,59 @@
+//  const school = 'RAJ UK Uttara Model School';
+//  console.log(school.toLocaleLowerCase());
+  
+//  const subject ='Chemistry';
+//  const book = 'chemistry';
+//  if (subject.toLocaleLowerCase() === book.toLocaleLowerCase()){
+//     console.log('This is my book i am reading book properly');
+//  }else{
+//     console.log('iam not interest to read book');
+//  }
+
+ const address = 'pirozali moytapar';
+ const part = address.slice(2,5);
+//  console.log(part);
+
+ const sentence ='I am a good and hardworking person ';
+//  console.log(sentence.split(' '));
+//  console.log(sentence.split('a'));
+// const friendsStr = 'Rahim,karim,selim,jelim,lahim,sahin';
+// const friends = friendsStr.split(',');
+// console.log(friends);
+
+// const realFriend = [ 'Rahim', 'karim', 'selim', 'jelim', 'lahim', 'sahin' ];
+// console.log(realFriend.join('-'));
+
+
+// concat trying this method
+
+const first = 'Mehide';
+const last = 'Hasan';
+
+const fullName = first + last;
+// console.log(fullName);
+
+const fullName2 = first.concat(' ').concat(last)
+// console.log(fullName2);
+
+// .includes() method
+console.log(last.includes('s'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ string.js
@@ -0,0 +1,15 @@
+const country = 'Bangladesh';
+const division = "Dhaka";
+const distict = `B-Baria`;
+// console.log(typeof country);
+
+// Array is mutable ----> you can change the array elements
+
+const numbers =[54,35,25,26,77]
+// console.log(numbers.length);
+// console.log(numbers[1]);
+
+//  string is immutable ---> no changeable
+const capital = 'dhaka';
+console.log(capital.length);
+console.log(capital[3]);
