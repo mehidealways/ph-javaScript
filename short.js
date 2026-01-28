6438b3990fdfd81f84ae19b3bb4dd8c1a9f6c4f6
mehidealways <mehidehasanmilon444@gmail.com>
2026-01-28 22:38:17 +0400

    how does work sort method

--- short.js
+++ short.js
@@ -1,3 +1,16 @@
 const persons = ['mahin','hasan','manik','mahabub','milon'];
 
 const sortPerson = persons.sort();
+console.log(sortPerson);
+// sort --->
+// const numbers =[1,2,4,5,6,7];
+/**
+ * Ascending ----> smaller to larger.
+ * Descending ----> larger to smaller
+ */
+const numbers =[,12,4,53,6,17,1];
+// const numbers_asc = numbers.sort(); --> not working porperly
+// const numbers_asc = numbers.sort(function (a,b){a-b});
+const numbers_asc = numbers.sort(function (a,b){return a-b});
+const numbers_dsc = numbers.sort(function (a,b){return a-b});
+console.log(numbers_asc);
