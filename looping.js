d0075b27f944041bd38f0adcdf0a5fc661e90f2d
mehidealways <mehidehasanmilon444@gmail.com>
2026-01-28 20:43:53 +0400

    useing loop in array how does work

---
+++ looping.js
@@ -0,0 +1,34 @@
+
+
+const friends = ['mehide','Hasan','Milon','Atik'];
+
+for (const friend of friends){
+    // console.log(friend);
+}
+
+for (let i =0;i< friends.length; i++){
+    // console.log(i);
+    // console.log(friends[i]);
+}
+
+const numbers = [11,22,33,44,55,66,77,88,99,];
+
+for (let i =0; i<numbers.length;i++){
+    // console.log(numbers[i]);
+}
+
+let i =0;
+while (i<friends.length){
+    // console.log(friends[i]);
+    i++;
+}
+
+let n = 0;
+while (n<numbers.length){
+    // console.log(numbers[n]);
+    n++;
+}
+
+
+
+// Reverse Array using loop
