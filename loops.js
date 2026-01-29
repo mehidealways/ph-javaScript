f1d7579e26560f916ffed92998b4d8b22629920e
mehidealways <mehidehasanmilon444@gmail.com>
2026-01-29 21:11:56 +0400

    Basic example for loop and for in loop

--- loops.js
+++ loops.js
@@ -11,5 +11,28 @@ const fruits = ['orange', 'banana', 'apple', 'jackfruit', 'Watermelon'];
 
 for (const fruit of fruits) {
   console.log(fruit);
-  console.log('I want to eat');
+  // console.log('I want to eat');
 }
+
+
+
+
+
+
+
+// <-----Looop with summit------>
+
+// for(statement aria, condition ,incress/decress){console.log()}
+
+/**
+ * for(let i =0; i < 10 ;i++){
+  console.log('This is For Loop:',i);
+}
+
+// <---- for In loop ----->
+const person = {fname:"Milon",lname:"Miah",age:25}
+for(let x in person){
+  console.log(person["fname"]);
+}
+  
+ */
